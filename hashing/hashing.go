e82cf64d4a9551e980d9a7caffaf4b1f027b3a5a
Sai Rithvik Ayithapu <rithvikayithapu@gmail.com>
2021-10-09 11:38:16 +0530

    Finished adding hashing and ids

---
+++ hashing/hashing.go
@@ -0,0 +1,50 @@
+package hashing
+
+import (
+	"crypto/rand"
+	"crypto/sha512"
+	"encoding/base64"
+)
+
+// Define salt size
+const saltSize = 16
+
+// Generate 16 bytes randomly and securely using the
+// Cryptographically secure pseudorandom number generator (CSPRNG)
+// in the crypto.rand package
+func generateRandomSalt(saltSize int) []byte {
+	var salt = make([]byte, saltSize)
+
+	_, err := rand.Read(salt[:])
+
+	if err != nil {
+		panic(err)
+	}
+
+	return salt
+}
+
+// Combine password and salt then hash them using the SHA-512
+// hashing algorithm and then return the hashed password
+// as a base64 encoded string
+func hashPassword(password string, salt []byte) string {
+	// Convert password string into bbyte slice
+	var passwordBytes = []byte(password)
+
+	// Create sha-512 hasher
+	var sha512Hasher = sha512.New()
+
+	// Append salt to password
+	passwordBytes = append(passwordBytes, salt...)
+
+	// Write password bytes to the hasher
+	sha512Hasher.Write(passwordBytes)
+
+	// Get the SHA-512 hashed password
+	var hashedPasswordBytes = sha512Hasher.Sum(nil)
+
+	// Convert the hashed password to a base64 encoded string
+	var base64EncodedPasswordHash = base64.URLEncoding.EncodeToString(hashedPasswordBytes)
+
+	return base64EncodedPasswordHash
+}
