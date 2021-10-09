fb17254ca0b9c4c02ef7ef1888321c62f04f754a
Sai Rithvik Ayithapu <rithvikayithapu@gmail.com>
2021-10-09 19:38:35 +0530

    Updated routes sto serve specific requests

--- posts/posts.go
+++ posts/posts.go
@@ -39,51 +39,56 @@ GetPost - collects posts from DB and matches ID
  of posts with the ID given in the link
 */
 func GetPost(w http.ResponseWriter, r *http.Request) {
-	// Split the URL to obtain the ID, store it in postId
-	p := strings.Split(r.URL.Path, "/")
-	var postId string
-	if len(p) <= 1 {
-		log.Fatal("id not found")
-		w.WriteHeader(http.StatusInternalServerError)
+	if r.Method == http.MethodGet {
+		// Split the URL to obtain the ID, store it in postId
+		p := strings.Split(r.URL.Path, "/")
+		var postId string
+		if len(p) <= 1 {
+			log.Fatal("id not found")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		} else {
+			postId = p[2]
+		}
+
+		// Initialize posts array and collect posts from DB
+		posts := []Post{}
+		cursor, err := collection.Find(context.TODO(), bson.D{{}})
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// Iterate through posts and match ID with postId
+		for cursor.Next(context.TODO()) {
+			var post Post
+			cursor.Decode(&post)
+			if post.Id == postId {
+				posts = append(posts, post)
+			}
+		}
+
+		// Convert the result to JSON
+		jsonBytes, err := json.Marshal(posts)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// Structure the response
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonBytes)
+
 		return
 	} else {
-		postId = p[2]
-	}
-
-	// Initialize posts array and collect posts from DB
-	posts := []Post{}
-	cursor, err := collection.Find(context.TODO(), bson.D{{}})
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-
-	// Iterate through posts and match ID with postId
-	for cursor.Next(context.TODO()) {
-		var post Post
-		cursor.Decode(&post)
-		if post.Id == postId {
-			posts = append(posts, post)
-		}
-	}
-
-	// Convert the result to JSON
-	jsonBytes, err := json.Marshal(posts)
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	// Structure the response
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
-
-	return
 }
 
 /*
@@ -92,57 +97,62 @@ creates ID, records timestamp and creates Post object.
 Post object is then converted to JSON and added to DB
 */
 func CreatePost(w http.ResponseWriter, r *http.Request) {
-	// Create PostInfo object
-	var post PostInfo
+	if r.Method == http.MethodPost {
+		// Create PostInfo object
+		var post PostInfo
 
-	// Decode the input JSON and match it with PostInfo struct
-	err := json.NewDecoder(r.Body).Decode(&post)
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err.Error()))
+		// Decode the input JSON and match it with PostInfo struct
+		err := json.NewDecoder(r.Body).Decode(&post)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// Generate ID and record timestamp
+		Id := generate.GenId()
+		Timestamp := time.Now().Format(time.RFC3339)
+
+		// Create Post object, name it newPost
+		newPost := Post{
+			Id:        Id,
+			User_id:   post.User_id,
+			Caption:   post.Caption,
+			Img_url:   post.Img_url,
+			Timestamp: Timestamp,
+		}
+
+		// Insert newPost into DB
+		res, err := collection.InsertOne(context.TODO(), newPost)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		log.Println(res) // Handling response from Mongo driver
+
+		// Convert newPost to JSON
+		jsonBytes, err := json.Marshal(newPost)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// Structure the response
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonBytes)
+
+		return
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-
-	// Generate ID and record timestamp
-	Id := generate.GenId()
-	Timestamp := time.Now().Format(time.RFC3339)
-
-	// Create Post object, name it newPost
-	newPost := Post{
-		Id:        Id,
-		User_id:   post.User_id,
-		Caption:   post.Caption,
-		Img_url:   post.Img_url,
-		Timestamp: Timestamp,
-	}
-
-	// Insert newPost into DB
-	res, err := collection.InsertOne(context.TODO(), newPost)
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	log.Println(res) // Handling response from Mongo driver
-
-	// Convert newPost to JSON
-	jsonBytes, err := json.Marshal(newPost)
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	// Structure the response
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
-
-	return
 }
 
 /*
@@ -151,49 +161,54 @@ ID of the user who posted it with the ID of given
 in the link
 */
 func GetUsersPost(w http.ResponseWriter, r *http.Request) {
-	// Split the URL to obtain the ID, store it in userId
-	p := strings.Split(r.URL.Path, "/")
-	var userId string
-	if len(p) <= 1 {
-		log.Fatal("id not found")
-		w.WriteHeader(http.StatusInternalServerError)
+	if r.Method == http.MethodGet {
+		// Split the URL to obtain the ID, store it in userId
+		p := strings.Split(r.URL.Path, "/")
+		var userId string
+		if len(p) <= 1 {
+			log.Fatal("id not found")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		} else {
+			userId = p[3]
+		}
+
+		// Initialize posts array and collect posts from DB
+		posts := []Post{}
+		cursor, err := collection.Find(context.TODO(), bson.D{{}})
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// Iterate through posts and match User_iD with userId
+		for cursor.Next(context.TODO()) {
+			var post Post
+			cursor.Decode(&post)
+			if post.User_id == userId {
+				posts = append(posts, post)
+			}
+		}
+
+		// Convert the result to JSON
+		jsonBytes, err := json.Marshal(posts)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// Structure the response
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonBytes)
+
 		return
 	} else {
-		userId = p[3]
-	}
-
-	// Initialize posts array and collect posts from DB
-	posts := []Post{}
-	cursor, err := collection.Find(context.TODO(), bson.D{{}})
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-
-	// Iterate through posts and match User_iD with userId
-	for cursor.Next(context.TODO()) {
-		var post Post
-		cursor.Decode(&post)
-		if post.User_id == userId {
-			posts = append(posts, post)
-		}
-	}
-
-	// Convert the result to JSON
-	jsonBytes, err := json.Marshal(posts)
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	// Structure the response
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
-
-	return
 }
--- users/users.go
+++ users/users.go
@@ -39,51 +39,56 @@ GetUser - collects users from DB and matches ID
  of users with the ID given in the link
 */
 func GetUser(w http.ResponseWriter, r *http.Request) {
-	// Split the URL to obtain the ID, store it in userId
-	p := strings.Split(r.URL.Path, "/")
-	var userId string
-	if len(p) <= 1 {
-		log.Fatal("id not found")
-		w.WriteHeader(http.StatusInternalServerError)
+	if r.Method == http.MethodGet {
+		// Split the URL to obtain the ID, store it in userId
+		p := strings.Split(r.URL.Path, "/")
+		var userId string
+		if len(p) <= 1 {
+			log.Fatal("id not found")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		} else {
+			userId = p[2]
+		}
+
+		// Initialize users array and collect users from DB
+		users := []User{}
+		cursor, err := collection.Find(context.TODO(), bson.D{{}})
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// Iterate through users and match ID with userId
+		for cursor.Next(context.TODO()) {
+			var user User
+			cursor.Decode(&user)
+			if user.Id == userId {
+				users = append(users, user)
+			}
+		}
+
+		// Convert the result to JSON
+		jsonBytes, err := json.Marshal(users)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// Structure the response
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonBytes)
+
 		return
 	} else {
-		userId = p[2]
-	}
-
-	// Initialize users array and collect users from DB
-	users := []User{}
-	cursor, err := collection.Find(context.TODO(), bson.D{{}})
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-
-	// Iterate through users and match ID with userId
-	for cursor.Next(context.TODO()) {
-		var user User
-		cursor.Decode(&user)
-		if user.Id == userId {
-			users = append(users, user)
-		}
-	}
-
-	// Convert the result to JSON
-	jsonBytes, err := json.Marshal(users)
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	// Structure the response
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
-
-	return
 }
 
 /*
@@ -92,61 +97,66 @@ creates ID, hashes password and creates User object.
 User object is then converted to JSON and added to DB
 */
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	// Create UserInfo object
-	var user UserInfo
+	if r.Method == http.MethodPost {
+		// Create UserInfo object
+		var user UserInfo
 
-	// Decode the input JSON and match it with UserInfo struct
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err.Error()))
+		// Decode the input JSON and match it with UserInfo struct
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// Retrieve salt from environment variables
+		salt, ok := os.LookupEnv("salt")
+		if ok != true {
+			log.Fatal("error: unable to find uri in the environment")
+		}
+
+		// Generate ID and hashed password
+		Id := generate.GenId()
+		newSalt := []byte(salt)
+		user.Password = hashing.HashPassword(user.Password, newSalt)
+
+		// Create User object, name it newUser
+		newUser := User{
+			Id:       Id,
+			Name:     user.Name,
+			Email:    user.Email,
+			Password: user.Password,
+		}
+
+		// Insert newUser into DB
+		res, err := collection.InsertOne(context.TODO(), newUser)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		log.Println(res) // Handling response from Mongo driver
+
+		// Convert newUser to JSON
+		jsonBytes, err := json.Marshal(newUser)
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		// Structure the response
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonBytes)
+
+		return
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-
-	// Retrieve salt from environment variables
-	salt, ok := os.LookupEnv("salt")
-	if ok != true {
-		log.Fatal("error: unable to find uri in the environment")
-	}
-
-	// Generate ID and hashed password
-	Id := generate.GenId()
-	newSalt := []byte(salt)
-	user.Password = hashing.HashPassword(user.Password, newSalt)
-
-	// Create User object, name it newUser
-	newUser := User{
-		Id:       Id,
-		Name:     user.Name,
-		Email:    user.Email,
-		Password: user.Password,
-	}
-
-	// Insert newUser into DB
-	res, err := collection.InsertOne(context.TODO(), newUser)
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	log.Println(res) // Handling response from Mongo driver
-
-	// Convert newUser to JSON
-	jsonBytes, err := json.Marshal(newUser)
-	if err != nil {
-		log.Fatal(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	// Structure the response
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
-
-	return
 }
